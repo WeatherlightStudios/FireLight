022d5425143035044a7e3dc4593d8087a8779aee
Giuliano Gyoshev <darkjeson55@abv.bg>
2017-09-02 05:25:45 -0700

    test1
    gitignore test

--- FireLight/src/Renderable.cpp
+++ FireLight/src/Renderable.cpp
@@ -71,11 +71,8 @@ void Renderable::generateTriangle()
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
-	// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+	
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
 	glBindVertexArray(0);
 }
 
