2fe6a3a59aa059b1abf8aae1445947ab44964bc4
Cogno_Barco <cognokill@gmail.com>
2017-09-28 15:41:59 +0200

    More Gameplay for Pong
    BugFix + Fisica con Racchette (ancora un bug da fixare)

--- Test/Game.cpp
+++ Test/Game.cpp
@@ -10,13 +10,13 @@ Game::Game() : FL::App("Hello", 800,600), m_camera(glm::vec3(0,0,-3), glm::vec3(
 void Game::Init()
 {
 	m_paddle1.Init();
-	m_paddle1.set_trasform(glm::vec3(1.3f, 0.0f,0.0f));
+	m_paddle1.set_trasform(glm::vec3(1.2f, 0.0f,0.0f));
 	m_paddle1.set_color(glm::vec3(0.0,1.0,0.0));
 	m_paddle1.set_scale(glm::vec3( 0.05f, 800.f/600.f / 4, 1.0f));
 	m_paddle1.gen_quad();
 
 	m_paddle2.Init();
-	m_paddle2.set_trasform(glm::vec3(-1.3f, 0.0f, 0.0f));
+	m_paddle2.set_trasform(glm::vec3(-1.2f, 0.0f, 0.0f));
 	m_paddle2.set_color(glm::vec3(0.0, 1.0, 0.0));
 	m_paddle2.set_scale(glm::vec3(0.05f, 800.f / 600.f / 4, 1.0f));
 	m_paddle2.gen_quad();
@@ -75,76 +75,103 @@ void Game::Update(double deltaT)
 	////PHYSICS SETUP////
 
 	//half wall position
-	float wall_pos_half_y = 1.0 - 800.f / 600.f / 4 * 0.5f;
-	float wall_pos_half_x = 1.0 - 600.f / 800.f / 4 * 0.5f;
+	float wall_pos_half_y_paddle = 1 - m_paddle1.get_scale().y * 0.5f;
+
+	float wall_pos_half_y_ball = 1 - m_ball.get_scale().y * 0.5f;
+	float wall_pos_half_x_ball = 1.3333 - m_ball.get_scale().x * 0.5f;
 
 	//ball pos
 	glm::vec3 ballPos = m_ball.get_position();
 
-	std::cout << ballPos.y << std::endl;
-
+	
 	////BALL PHYSICS////
 
 	////sfera contro muri
 
 	//muro sopra
-	if(m_ball.get_position().y > wall_pos_half_y)
+	if(m_ball.get_position().y > wall_pos_half_y_ball)
 	{
 		//mette palla nella posizione giusta
-		m_ball.set_trasform(glm::vec3(ballPos.x, wall_pos_half_y, ballPos.z));
+		m_ball.set_trasform(glm::vec3(ballPos.x, wall_pos_half_y_ball, ballPos.z));
 		//inverte velocità in asse y
 		ball_dy *= -1;
 	}
 
 	//muro sotto
-	if (m_ball.get_position().y < wall_pos_half_y * -1)
+	if (m_ball.get_position().y < wall_pos_half_y_ball * -1)
 	{
 		//mette palla nella posizione giusta
-		m_ball.set_trasform(glm::vec3(ballPos.x, wall_pos_half_y * -1, ballPos.z));
+		m_ball.set_trasform(glm::vec3(ballPos.x, wall_pos_half_y_ball * -1, ballPos.z));
 		//inverte velocità in asse y
 		ball_dy *= -1;
 	}
 
 	//muro destro
-	if (m_ball.get_position().x > wall_pos_half_x)
+	if (m_ball.get_position().x > wall_pos_half_x_ball)
 	{
 		//mette palla nella posizione giusta
-		m_ball.set_trasform(glm::vec3(wall_pos_half_x, ballPos.y, ballPos.z));
+		m_ball.set_trasform(glm::vec3(wall_pos_half_x_ball, ballPos.y, ballPos.z));
 		//inverte velocità in asse y
 		ball_dx *= -1;
 	}
 
 	//muro sinistro
-	if (m_ball.get_position().x < wall_pos_half_x * -1)
+	if (m_ball.get_position().x < wall_pos_half_x_ball * -1)
 	{
 		//mette palla nella posizione giusta
-		m_ball.set_trasform(glm::vec3(wall_pos_half_x * -1, ballPos.y, ballPos.z));
+		m_ball.set_trasform(glm::vec3(wall_pos_half_x_ball * -1, ballPos.y, ballPos.z));
 		//inverte velocità in asse y
 		ball_dx *= -1;
 	}
 
+	////pallina contro racchette
+	glm::vec3 ballMovement = glm::vec3(ball_dx * deltaT, ball_dy * deltaT, 0);
+
+	//scontro solo in x racchetta sinistra
+	if (ballPos.x + ballMovement.x > m_paddle1.get_position().x - m_paddle1.get_scale().x && ballPos.x + ballMovement.x < m_paddle1.get_position().x + m_paddle1.get_scale().x)
+	{
+		//scontro solo in y racchetta sinistra
+		if (ballPos.y + ballMovement.y < m_paddle1.get_position().y + m_paddle1.get_scale().y && ballPos.y + ballMovement.y > m_paddle1.get_position().y - m_paddle1.get_scale().y)
+		{
+			//cambia direzione
+			ball_dx *= -1;
+		}
+	}
+
+	//scontro solo in x racchetta destra
+	if (ballPos.x + ballMovement.x > m_paddle2.get_position().x - m_paddle2.get_scale().x && ballPos.x + ballMovement.x < m_paddle2.get_position().x + m_paddle2.get_scale().x)
+	{
+		//scontro solo in y racchetta destra
+		if (ballPos.y + ballMovement.y < m_paddle2.get_position().y + m_paddle2.get_scale().y && ballPos.y + ballMovement.y > m_paddle2.get_position().y - m_paddle2.get_scale().y)
+		{
+			//cambia direzione
+			ball_dx *= -1;
+		}
+	}
+
+
 	////racchetta contro muri
 
 	// border collision first paddle
-	if (m_paddle1.get_position().y > wall_pos_half_y)
+	if (m_paddle1.get_position().y > wall_pos_half_y_paddle)
 	{
-		m_paddle1.set_trasform(glm::vec3(1.3, wall_pos_half_y, 0));
+		m_paddle1.set_trasform(glm::vec3(1.2, wall_pos_half_y_paddle, 0));
 	}
 
-	if (m_paddle1.get_position().y < wall_pos_half_y * -1)
+	if (m_paddle1.get_position().y < wall_pos_half_y_paddle * -1)
 	{
-		m_paddle1.set_trasform(glm::vec3(1.3, wall_pos_half_y * -1, 0));
+		m_paddle1.set_trasform(glm::vec3(1.2, wall_pos_half_y_paddle * -1, 0));
 	}
 
 	//border collision second paddle
-	if (m_paddle2.get_position().y > wall_pos_half_y)
+	if (m_paddle2.get_position().y > wall_pos_half_y_paddle)
 	{
-		m_paddle2.set_trasform(glm::vec3(-1.3, wall_pos_half_y, 0));
+		m_paddle2.set_trasform(glm::vec3(-1.2, wall_pos_half_y_paddle, 0));
 	}
 
-	if (m_paddle2.get_position().y < wall_pos_half_y * -1)
+	if (m_paddle2.get_position().y < wall_pos_half_y_paddle * -1)
 	{
-		m_paddle2.set_trasform(glm::vec3(-1.3, wall_pos_half_y * -1, 0));
+		m_paddle2.set_trasform(glm::vec3(-1.2,  wall_pos_half_y_paddle * -1, 0));
 	}
 
 }
