d3880927ee1a83a60bb29f24344c5cb0dca5a106
Cogno_Barco <cognokill@gmail.com>
2017-11-17 18:11:21 +0100

    Slime Walk + sprite scaling
    +possibilità di cambiare dimensione delle sprite
    +slime sprite
    +slime in game
    +movimento slime (dopo un piccolo timer si lancia in direzione del giocatore rallentando progressivamente)

--- Test/TestScene.cpp
+++ Test/TestScene.cpp
@@ -5,6 +5,7 @@
 TestScene::TestScene()
 {
 	player = new Renderable;
+	slime = new Renderable;
 }
 
 void TestScene::Init()
@@ -13,22 +14,37 @@ void TestScene::Init()
 
 	//gl3wInit();
 	//ResourceManager::LoadTexture("sprite.png", true, "sprite");
+	//imposta sfondo della finestra
 	glClearColor(192.f/255, 192.f/255, 192.f/255, 1);
-	//carica sprite e la salva con nome "sprite"
-	ResourceManager::LoadTexture("Sprites/Paladino_Walk_OLD.png", true, "sprite");
-	//applica texture a player
-	player->set_texture("sprite");
-	//numero di righe e colonne
+	//carica sprites e salva con un nome
+	ResourceManager::LoadTexture("Sprites/Paladino_Walk_OLD.png", true, "player");
+	ResourceManager::LoadTexture("Sprites/Slime.png", true, "slime");
+	//applica texture a un Renderable
+	player->set_texture("player");
+	slime->set_texture("slime");
+	//numero di righe e colonne per ogni sprite
 	player->set_texture_row(glm::vec2(ROW, COLUMNS));
+	slime->set_texture_row(glm::vec2(1, 1));//per ora c'è solo 1 riga e 1 colonna
 	//aggiunge a scena
 	addObject(player);
+	addObject(slime);
+
+	player->set_local_scale(glm::vec3(0.35, 0.35, 0.35));
+	slime->set_local_scale(glm::vec3(0.35, 0.35, 0.35));
+
+	//slime->set_local_position(glm::vec3(0.5, 0.5, 0));
+
+	//slime setup
+	slimeChangingSpeed = slimeSpeed;
 }
 void TestScene::Update(double dt)
 {
 	
+	///PLAYER///
+
 	//player movement
 	glm::vec3 playerPos = player->get_world_position();
-
+	
 	currentCenterAxisX = GetAxis(GLFW_KEY_D, GLFW_KEY_A, GRAVITY, currentCenterAxisX);
 	currentCenterAxisY = GetAxis(GLFW_KEY_W, GLFW_KEY_S, GRAVITY, currentCenterAxisY);
 
@@ -74,6 +90,50 @@ void TestScene::Update(double dt)
 	player->set_local_position(playerPos);
 	//animazione
 	player->set_texture_offset(glm::vec2(glm::round(xAnim),glm::round(yAnim)));
+
+
+	///SLIME///
+	//posizione slime
+	glm::vec3 slimePos = slime->get_world_position();
+
+	//constantemente diminuisce la velocità dello slime
+	slimeChangingSpeed -= speedDecrease * dt;
+
+	//se la velocità dello slime è sotto lo zero fa partire il timer e blocca il valore
+	//altrimenti resetta timer
+	if (slimeChangingSpeed > 0) {
+		timer = slimeLaunchTimer;
+	}
+	else {
+		slimeChangingSpeed = 0;
+		timer -= dt;
+	}
+
+	std::cout << "slimeChangingSpeed: " << slimeChangingSpeed << " timer: " << timer <<
+		"speedDecrease: " << speedDecrease << std::endl;
+
+	//quando timer è sotto a 0 fa uno scatto, quindi resetta la velocità dello slime
+	if (timer <= 0) {
+
+		//direzione da slime a giocatore
+		playerDir = playerPos - slimePos;
+
+		//glm::project()
+		//ignora asse z
+		playerDir.z = 0;
+		//proiezione sul piano con modulo 1 (se tutti diversi da 0)
+		if (glm::length(playerDir) != 0)
+			playerDir = glm::normalize(playerDir);
+
+
+		slimeChangingSpeed = slimeSpeed;
+	}
+
+	//aggiornamento posizione slime
+	glm::vec3 newSlimePos = slimePos + playerDir * slimeChangingSpeed * (float)dt;
+	slime->set_local_position(newSlimePos);
+
+
 }
 
 ///INPUT SYSTEM///
--- Test/TestScene.h
+++ Test/TestScene.h
@@ -11,22 +11,20 @@ public:
 	void Close();
 	~TestScene();
 private:
+	//game objects
 	Renderable *player;
+	Renderable *slime;
 
+	//constants for animations
+		//player
 	const float ROW = 5;
 	const float COLUMNS = 6;
 	const float ANIM_SPEED = 4;
-	
-	//INPUT VARIABLES
-	const float GRAVITY = 0.1;
-	const float AXIS_CENTER = 0;
-	const float AXIS_EDGE = 1;
-	float currentCenterAxisX = 0;
-	float currentCenterAxisY = 0;
-	//INPUT FUNCTION
-	float GetAxis(int PosKey, int NegKey, float Gravity, float x);
-	
+		//slime
+	//still nothing
+
 	//ANIMATION VARIABLES
+		//player
 	//x divisions
 	const int IDLE_X = 0;
 	const int WALK_START_X = 1;
@@ -38,13 +36,33 @@ private:
 	const int UP_Y = 3;
 	const int UL_Y = 4;
 	const int UR_Y = 5;
-
-	float xSpeed = 1;
-	float ySpeed = 1;
-
+	//speed variables
 	float xAnim, yAnim;
 	float xAnimSpeed = 4;
 	float yAnimSpeed = 1;
+		//slime
+	//still nothing
+
+	//INPUT VARIABLES
+	const float GRAVITY = 0.1;
+	const float AXIS_CENTER = 0;
+	const float AXIS_EDGE = 1;
+	float currentCenterAxisX = 0;
+	float currentCenterAxisY = 0;
+	//INPUT FUNCTION
+	float GetAxis(int PosKey, int NegKey, float Gravity, float x);
+	//player speed
+	float xSpeed = 1;
+	float ySpeed = 1;
+	
+	//slime
+	float slimeSpeed = 0.8;
+	float slimeChangingSpeed;
+	float speedDecrease = 0.75;
+	double slimeLaunchTimer = 0.5;
+	double timer = 0;
+	glm::vec3 playerDir;
+
 
 };
 
